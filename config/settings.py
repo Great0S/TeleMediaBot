b834deeffef5d1d7f2b8d5a2cc81535fc64620ae
Great0S <24699619+Great0S@users.noreply.github.com>
2024-05-31 22:55:46 +0300

    added .env

--- config/settings.py
+++ config/settings.py
@@ -3,16 +3,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
     
 ***REMOVED***
-***REMOVED***
-    alert_channel_id: int = -1001834749560
-    alert_bot_token: str = "5860981689:AAGBF84N0n7rBPNz9TX_ZtB9bdvpyd4Vdbw"
-    alert_bot_user: str = "bf_bots_info"
-    
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
@@ -22,36 +18,41 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-
-***REMOVED***
-    api_id: int = 7148663
-    api_hash: str = "81c16de88cd5e25fcbf01e5af332b41f"
-    alert_api_id: int = 9859004
-    alert_api_hash: str = "9c813daded173c825a1ca2e618063089"
-
-***REMOVED***
-    username: str = "albeyanfashion2"
-    phone: int = 905434050709
-    token: str = "5606280453:AAHKKi33s_F-H5Cymh0aAT25dEatHKPJYDc"
-    bot_id: str = "@CocukStoreBot"
-***REMOVED***
-
-***REMOVED***
-    kids_id: list = [-1001338146588, -1001653408221]
-
+    
 ***REMOVED***
 ***REMOVED***
-
 ***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-    ecwid_token: str = "?token=secret_4i936SRqRp3317MZ51Aa4tVjeUVyGwW7"
 ***REMOVED***
 ***REMOVED***
-    "Authorization": "Bearer secret_4i936SRqRp3317MZ51Aa4tVjeUVyGwW7",
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
